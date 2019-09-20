3c4b2484c440b3a66acc977a487aa139defbeecc
Edwin van den Belt <bte@dat.nl>
2019-09-20 13:10:37 +0200

    depricated creation of swagger.yaml. Now in 1 file

--- tools/concatSwaggerparts.bat
+++ tools/concatSwaggerparts.bat
@@ -1,16 +1,18 @@
-
-cd ./work
-del *.yaml
-cd ..
-
-cd ../paths
-type *.* > ..\tools\work\paths.yaml
-cd ../components/schemas
-type *.* > ..\..\tools\work\schemas.yaml
-cd ..
-type *.* > ..\tools\work\components.yaml
-cd ..\tools\work\
-
-type ..\..\header ..\start.yaml paths.yaml ..\componentsStart.yaml schemas.yaml > swagger.yaml
-copy swagger.yaml ..
-cd ..
+@echo off
+echo deprecated. The swagger.yaml in the root directory should be used.
+pause
+rem cd ./work
+rem del *.yaml
+rem cd ..
+rem 
+rem cd ../paths
+rem type *.* > ..\tools\work\paths.yaml
+rem cd ../components/schemas
+rem type *.* > ..\..\tools\work\schemas.yaml
+rem cd ..
+rem type *.* > ..\tools\work\components.yaml
+rem cd ..\tools\work\
+rem 
+rem type ..\..\header ..\start.yaml paths.yaml ..\componentsStart.yaml schemas.yaml > swagger.yaml
+rem copy swagger.yaml ..
+rem cd ..
